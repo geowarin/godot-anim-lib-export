1555ebfe8949bb22014e4af3abbac045e3e9bc7d
bstriker <strikeforcezero@gmail.com>
2023-08-14 18:57:21 -0400

    clear .godot cache each run to ensure changes in generated glb are processed

--- gd_make_lib.sh
+++ gd_make_lib.sh
@@ -10,6 +10,8 @@ ANIMATIONS_DIR=${2}
 BASE_NAME=$(basename "$2")
 OUTPUT_FILE=${3:-$BASE_NAME}
 
+rm -rf $DIR/gd-proj-template/.godot
+
 "$DIR/mixamo_to_glb.sh" "$CHARACTER_FILE" "$ANIMATIONS_DIR" "$DIR/gd-proj-template/anims.glb"
 
 "$DIR/gd_gltf_to_res.sh" "$OUTPUT_FILE"
