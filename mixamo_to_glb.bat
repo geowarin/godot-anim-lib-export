b490cc3d23a730bc441f767888e126bfb1b55477
bstriker <strikeforcezero@gmail.com>
2023-08-15 00:58:02 -0400

    stop overwriting OUTPUT_FILE in child script

--- mixamo_to_glb.bat
+++ mixamo_to_glb.bat
@@ -8,13 +8,13 @@ set CHARACTER_FILE="%~1"
 set ANIMATIONS_DIR="%~2"
 
 for %%i in (%2) do set BASE_NAME=%%~nxi
-set OUTPUT_FILE=%3
-if "%OUTPUT_FILE%"=="" set OUTPUT_FILE=%BASE_NAME%
+set GLB_OUTPUT_FILE=%3
+if "%OUTPUT_FILE%"=="" set GLB_OUTPUT_FILE=%BASE_NAME%
 
-echo Generating %OUTPUT_FILE% with character %CHARACTER_FILE% with animations from %ANIMATIONS_DIR%
+echo Generating %GLB_OUTPUT_FILE% with character %CHARACTER_FILE% with animations from %ANIMATIONS_DIR%
 
 set GDLIB_CHARACTER_FILE=%CHARACTER_FILE%
 set GDLIB_ANIMATIONS_DIR=%ANIMATIONS_DIR%
-set GDLIB_OUTPUT_FILE=%OUTPUT_FILE%
+set GDLIB_OUTPUT_FILE=%GLB_OUTPUT_FILE%
 
 blender.exe --background -noaudio -P "%DIR%\mixamo_to_glb.py"
